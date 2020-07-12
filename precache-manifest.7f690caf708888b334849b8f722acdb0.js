85e172c3f92527fee1915cc54812c5ff1510cb55
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2020-07-12 10:38:31 +0100

    Updates

---
+++ precache-manifest.7f690caf708888b334849b8f722acdb0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336eedcc0c58b016efebe8995694f785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b232b2d8d473c852b22",
+    "url": "/static/css/main.13d0a309.chunk.css"
+  },
+  {
+    "revision": "1200558c3a1a0adc87ad",
+    "url": "/static/js/2.685aa11b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.685aa11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b232b2d8d473c852b22",
+    "url": "/static/js/main.39b978b3.chunk.js"
+  },
+  {
+    "revision": "bae492c776d6e417a041",
+    "url": "/static/js/runtime-main.1b461360.js"
+  },
+  {
+    "revision": "630661b22ac5e7bb70d83757dc195958",
+    "url": "/static/media/black.630661b2.png"
+  },
+  {
+    "revision": "4b46210e283e5b0cb191618b3c8eb8a5",
+    "url": "/static/media/blue.4b46210e.png"
+  },
+  {
+    "revision": "671f7afd8afe7a8019c968a3f8e28ad3",
+    "url": "/static/media/green.671f7afd.png"
+  },
+  {
+    "revision": "b12a7d0ec4a4c3aa47d8e454758907d4",
+    "url": "/static/media/purp.b12a7d0e.png"
+  }
+]);
